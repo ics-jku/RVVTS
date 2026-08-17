ics-jku/RVVTS

commit 7ae59a33b60d3baa87fc50ec9116de2b9a3da05e

rvvts: risc-v isa config: major rework from simple string class RVISACfg
 * can handle z.. extensions
 * includes many generalizations and cleanups

Signed-off-by: Manfred SCHLAEGL <manfred.schlaegl@gmx.at>

rvvts/AraRunner.py
@@ -14,9 +14,6 @@ class AraRunner(ProcessTimeoutRunner):
     def setup(self, config=None):
 
         super().setup(config=config)
-        self.config = config
-        self.rv_extensions = config["rv_extensions"]
-        self.xlen = config["xlen"]
 
         # ARA may hang on test case execution (running clock, but no instructions retired)
         # with this we control, whether we count such cases as TIMEOUT or ERROR (with lastPC = -1)

CovGuidedFuzzerGenRunnerTests.ipynb
@@ -67,12 +67,15 @@
     "config.update(dict(\n",
     "    dir = os.getcwd() + \"/run/CovGuidedFuzzerGenRunner\",\n",
     "\n",
+    "    # enable tests for all extensions supported by rvvts\n",
+    "    #rvisacfg = rvvts.RVISACfg(xlen = xlen, extensions_under_test = [\"b\", \"zbc\", \"v\"], vlen = vector_vlen, velen = 64),\n",
+    "    # enable tests for RVV only\n",
+    "    rvisacfg = rvvts.RVISACfg(xlen = xlen, extensions_under_test = [\"v\"], vlen = vector_vlen, velen = 64),\n",
+    "\n",
     "    RefCovRunner_coverage = rvvts.RISCVOVPSIMCoverageRunner,\n",
+    "    #RISCVOVPSIMCover_metric = \"mnemonic\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_metric = \"basic\", # basic, extended or mnemonic\n",
     "    RISCVOVPSIMCover_metric = \"extended\", # basic, extended or mnemonic\n",
-    "\n",
-    "    xlen = xlen,\n",
-    "    vector_vlen = vector_vlen,\n",
     "))\n",
     "\n",
     "if ref == \"Spike\":\n",

CovGuidedTestsetGenerator.ipynb
@@ -92,12 +92,15 @@
     "config.update(dict(\n",
     "    dir = DIR,\n",
     "\n",
+    "    # enable tests for all extensions supported by rvvts\n",
+    "    #rvisacfg = rvvts.RVISACfg(xlen = xlen, extensions_under_test = [\"b\", \"zbc\", \"v\"], vlen = RVV_VLEN, velen = 64),\n",
+    "    # enable tests for RVV only\n",
+    "    rvisacfg = rvvts.RVISACfg(xlen = RV_XLEN, extensions_under_test = [\"v\"], vlen = RVV_VLEN, velen = 64),\n",
+    "\n",
     "    RefCovRunner_coverage = rvvts.RISCVOVPSIMCoverageRunner,\n",
+    "    #RISCVOVPSIMCover_metric = \"mnemonic\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_metric = \"basic\", # basic, extended or mnemonic\n",
     "    RISCVOVPSIMCover_metric = \"extended\", # basic, extended or mnemonic\n",
-    "\n",
-    "    xlen = RV_XLEN,\n",
-    "    vector_vlen = RVV_VLEN,\n",
     "))\n",
     "\n",
     "if REF == \"Spike\":\n",

FuzzCodeErrMinRunnerTests.ipynb
@@ -80,14 +80,17 @@
     "\n",
     "    CompareRunner_dut = rvvts.DuTGDBRunner,\n",
     "\n",
+    "    # enable tests for all extensions supported by rvvts\n",
+    "    #rvisacfg = rvvts.RVISACfg(xlen = xlen, extensions_under_test = [\"b\", \"zbc\", \"v\"], vlen = vector_vlen, velen = 64),\n",
+    "    # enable tests for RVV only\n",
+    "    rvisacfg = rvvts.RVISACfg(xlen = xlen, extensions_under_test = [\"v\"], vlen = vector_vlen, velen = 64),\n",
+    " \n",
     "    # Optional: Coverage\n",
     "    #RefCovRunner_coverage = rvvts.RISCVOVPSIMCoverageRunner,\n",
+    "    #RISCVOVPSIMCover_metric = \"mnemonic\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_metric = \"basic\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_metric = \"extended\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_sum_enable = True,\n",
-    "    \n",
-    "    xlen = xlen,\n",
-    "    vector_vlen = vector_vlen,\n",
     "))\n",
     "\n",
     "if ref == \"Spike\":\n",

TestsetCodeErrMinRunnerTests.ipynb
@@ -90,14 +90,17 @@
     "\n",
     "    CompareRunner_dut = rvvts.DuTGDBRunner,\n",
     "\n",
+    "    # enable tests for all extensions supported by rvvts\n",
+    "    #rvisacfg = rvvts.RVISACfg(xlen = xlen, extensions_under_test = [\"b\", \"zbc\", \"v\"], vlen = vector_vlen, velen = 64),\n",
+    "    # enable tests for RVV only\n",
+    "    rvisacfg = rvvts.RVISACfg(xlen = xlen, extensions_under_test = [\"v\"], vlen = vector_vlen, velen = 64),\n",
+    " \n",
     "    # Optional: Coverage\n",
     "    #RefCovRunner_coverage = rvvts.RISCVOVPSIMCoverageRunner,\n",
+    "    #RISCVOVPSIMCover_metric = \"mnemonic\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_metric = \"basic\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_metric = \"extended\", # basic, extended or mnemonic\n",
     "    #RISCVOVPSIMCover_sum_enable = True,\n",
-    "    \n",
-    "    xlen = xlen,\n",
-    "    vector_vlen = vector_vlen,\n",
     "))\n",
     "\n",
     "if ref == \"Spike\":\n",

config_base.py
@@ -7,6 +7,8 @@
 # Global base configuration for RVVTS
 # (Overrride in custom config; only change if you know what you are doing!)
 
+from rvvts import RVISACfg
+
 memstart = 0x80000000 # compatible with spike, qemu, riscv-vp++ and ara
 #memlen = 512*1024 # 512KiB
 memlen = 3*1024*1024 # 3MiB
@@ -68,8 +70,8 @@ config = dict(
     # WARNING: B in OVPSim is outdated! -> not really useful
     #RISCVOVPSIMCover_extensions = "B",
 
-    rv_extensions = "mfdv",
-    vector_elen = 64,
+    # enable tests for all extensions supported by rvvts
+    rvisacfg = RVISACfg(xlen = 64, extensions_under_test = ["b", "zbc", "v"], vlen = 128, velen = 64),
 
     memstart = memstart,
     memlen = memlen,

rvvts/BuildRunner.py
@@ -19,30 +19,20 @@ class BuildRunner(ProcessTimeoutRunner):
         skip_on_exception = config["skip_on_exception"]
         xmemstart = config["xmemstart"]
         xmemlen = config["xmemlen"]
-        rv_extensions = config["rv_extensions"] + "_zicsr_zifencei"
+        rvisacfg = config["rvisacfg"]
+        xlen = rvisacfg.get_xlen()
 
-        xlen = config["xlen"]
-        self.xlenb = xlen // 8
         if xlen == 32:
-            march = "rv32i" + rv_extensions
+            march = rvisacfg.to_isa_str()
             mabi = "ilp32"
         elif xlen == 64:
-            march = "rv64i" + rv_extensions
+            march = rvisacfg.to_isa_str()
             mabi = "lp64"
         else:
-            raise Exception(
-                "xlen=" + str(xlen) + " not supported! Valid values are 32, or 64"
-            )
+            raise Exception(f"xlen = {xlen} not supported! Valid values are 32, or 64")
 
-        rv_extensions = config["rv_extensions"]
-        has_float_single = "f" in rv_extensions
-        has_float_double = "d" in rv_extensions
-        has_float_quad = "q" in rv_extensions
-        if has_float_quad:
-            raise Exception("Quad-precision floating-point not supported yet!")
-        has_float = has_float_single or has_float_double or has_float_quad
-
-        has_vector = "v" in rv_extensions
+        has_float = rvisacfg.is_float_needed()
+        has_vector = rvisacfg.is_needed("v")
 
         if self.log:
             self.codefile = RunnerFile(dir=self.get_dir(), name="code.S")
@@ -249,15 +239,9 @@ _01_testcode_init_exec:
 """
 
         if has_float:
-            if has_float_single:
-                instr = "fcvt.s.w"
-            if has_float_double:
-                instr = "fcvt.d.w"
-            if has_float_quad:
-                instr = "fcvt.q.w"
             self.asmhdr += "    # init float registers\n"
             for i in range(0, 32):
-                self.asmhdr += "    " + instr + " f" + str(i) + ", zero\n"
+                self.asmhdr += f"    {rvisacfg.get_fset_max()} f{i}, zero\n"
 
         if has_vector:
             self.asmhdr += """

rvvts/CodeErrMinRunner.py
@@ -68,7 +68,6 @@ def code_minimize(
     code: CodeBlock,
     good_idx,
     bad_idx,
-    rv_extensions="",
     **kwargs,
 ):
 
@@ -142,8 +141,6 @@ class CodeErrMinRunner(Runner):
     def setup(self, config):
 
         super().setup(config)
-
-        self.rv_extensions = config["rv_extensions"]
 
         self.CODE_STATUS_EXECUTED = "0: executed"
         self.CODE_STATUS_REDUCED = "1: reduced"
@@ -258,7 +255,6 @@ class CodeErrMinRunner(Runner):
             code_minimize(
                 codecheckrunner=self.codecheckrunner,
                 codecomparerunner=self.codecomparerunner_min,
-                rv_extensions=self.rv_extensions,
                 code=code_block,
                 good_idx=good_idx,
                 bad_idx=bad_idx,

commit 144a6d3b1a6e30aed5dcb45cad7eb9896b2422ff

rvvts: AraRunner: fixed transition to RVISACfg
We accidentally broke AraRunner with
7ae59a33b60d3baa87fc50ec9116de2b9a3da05e
("rvvts: risc-v isa config: major rework from simple string class RVISACfg")
by removing the "config" class member.

fixed by re-adding "config"

Signed-off-by: Manfred SCHLAEGL <manfred.schlaegl@gmx.at>

rvvts/AraRunner.py
@@ -14,6 +14,7 @@ class AraRunner(ProcessTimeoutRunner):
     def setup(self, config=None):
 
         super().setup(config=config)
+        self.config = config
 
         # ARA may hang on test case execution (running clock, but no instructions retired)
         # with this we control, whether we count such cases as TIMEOUT or ERROR (with lastPC = -1)